3e109da0efbb4bd130591f036f46b7d8bd07e5e8
kwon.yuchan <kwon.yuchan@nbplus.kr>
2018-03-01 00:08:15 +0900

    service

--- public/angular/loc8rApp.js
+++ public/angular/loc8rApp.js
@@ -1,10 +1,10 @@
 angular.module('loc8rApp', []);
 
-var _isNumeric = function(n) {
+var _isNumeric = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-var formatDistance = function() {
+var formatDistance = function () {
   return function (distance) {
     var numDistance, unit;
     if (distance && _isNumeric(distance)) {
@@ -24,27 +24,17 @@ var formatDistance = function() {
   }
 };
 
-var locationListCtrl = function ($scope) {
-  $scope.data = {
-    locations: [{
-      name: 'Burger Queen',
-      address: '125 High Street, Reading, RG6 1PS',
-      rating: 3,
-      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-      distance: '0.296456',
-      _id: '5370a35f2536f6785f8dfb6a'
-    }, {
-      name: 'Costy',
-      address: '125 High Street, Reading, RG6 1PS',
-      rating: 5,
-      facilities: ['Hot drinks', 'Food', 'Alcoholic drinks'],
-      distance: '0.7865456',
-      _id: '5370a35f2536f6785f8dfb6a'
-    }]
-  };
+var locationListCtrl = function ($scope, loc8rData) {
+  loc8rData.then(function (data) {
+    $scope.data = {
+      locations: data.data
+    };
+  }).catch(function (e) {
+    console.log(e);
+  });
 };
 
-var ratingStars = function() {
+var ratingStars = function () {
   return {
     scope: {
       thisRating: '=rating'
@@ -53,8 +43,13 @@ var ratingStars = function() {
   };
 };
 
+var loc8rData = function ($http) {
+  return $http.get('/api/locations?lng=-0.9630884&lat=51.451041&maxDistance=20');
+}
+
 angular
   .module('loc8rApp')
   .controller('locationListCtrl', locationListCtrl)
   .filter('formatDistance', formatDistance)
-  .directive('ratingStars', ratingStars);
+  .directive('ratingStars', ratingStars)
+  .service('loc8rData', loc8rData);
