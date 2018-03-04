c9f3956db30347d2c8df4e7b56fcda2b4cf6079a
kwon.yuchan <kwon.yuchan@nbplus.kr>
2018-03-04 10:23:29 +0900

    filter directive

---
+++ app_client/common/directive/ratingStars/ratingStars.directive.js
@@ -0,0 +1,13 @@
+angular
+  .module('loc8rApp')
+  .directive('ratingStars', ratingStars);
+
+function ratingStars() {
+  return {
+    restrict: 'EA',   // E:element A:attribute C:class M:comment
+    scope: {
+      thisRating: '=rating'
+    },
+    templateUrl: '/common/directives/ratingStars/ratingStars.template.html'
+  };
+};
