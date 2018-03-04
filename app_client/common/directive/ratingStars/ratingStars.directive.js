09bccce706b1cb1dca6803d8ca019a2d3cc66383
kwon.yuchan <kwon.yuchan@nbplus.kr>
2018-03-04 11:21:25 +0900

    IIFE

--- app_client/common/directive/ratingStars/ratingStars.directive.js
+++ app_client/common/directive/ratingStars/ratingStars.directive.js
@@ -1,13 +1,15 @@
-angular
-  .module('loc8rApp')
-  .directive('ratingStars', ratingStars);
+(function () {
+  angular
+    .module('loc8rApp')
+    .directive('ratingStars', ratingStars);
 
-function ratingStars() {
-  return {
-    restrict: 'EA',   // E:element A:attribute C:class M:comment
-    scope: {
-      thisRating: '=rating'
-    },
-    templateUrl: '/common/directives/ratingStars/ratingStars.template.html'
-  };
-};
+  function ratingStars() {
+    return {
+      restrict: 'EA',   // E:element A:attribute C:class M:comment
+      scope: {
+        thisRating: '=rating'
+      },
+      templateUrl: '/common/directives/ratingStars/ratingStars.template.html'
+    };
+  }
+})();
--- app_client/home/home.controller.js
+++ app_client/home/home.controller.js
@@ -1,42 +1,44 @@
-angular
-  .module('loc8rApp')
-  .controller('homeCtrl', homeCtrl);
+(function () {
+  angular
+    .module('loc8rApp')
+    .controller('homeCtrl', homeCtrl);
 
-function homeCtrl($scope, loc8rData, geolocation) {
-  var vm = this;
-  vm.pageHeader = {
-    title: 'Loc8r',
-    strapline: 'Find places to work with wifi near you!'
-  };
-  vm.sidebar = {
-    content: 'Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you\'re looking for.'
-  };
+  function homeCtrl($scope, loc8rData, geolocation) {
+    var vm = this;
+    vm.pageHeader = {
+      title: 'Loc8r',
+      strapline: 'Find places to work with wifi near you!'
+    };
+    vm.sidebar = {
+      content: 'Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you\'re looking for.'
+    };
 
-  vm.message = 'Checking your location';
-  vm.getData = function (position) {
-    var lat = position.coords.latitude;
-    var lng = position.coords.longitude;
-    vm.message = 'Searching for nearby places';   
-    loc8rData.locationByCoords(lat, lng).then(function (data) {
-      vm.message = data.data.length > 0 ? '' : 'No locations found';
-      vm.data = {
-        locations: data.data
-      };
-    }).catch(function (e) {
-      vm.message = 'Sorry, something\'s gone wrong';
-    });
-  };
-  vm.showError = function (error) {
-    $scope.$apply(function () {
-      vm.message = error.message;
-    });
-  };
+    vm.message = 'Checking your location';
+    vm.getData = function (position) {
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+      vm.message = 'Searching for nearby places';   
+      loc8rData.locationByCoords(lat, lng).then(function (data) {
+        vm.message = data.data.length > 0 ? '' : 'No locations found';
+        vm.data = {
+          locations: data.data
+        };
+      }).catch(function (e) {
+        vm.message = 'Sorry, something\'s gone wrong';
+      });
+    };
+    vm.showError = function (error) {
+      $scope.$apply(function () {
+        vm.message = error.message;
+      });
+    };
 
-  vm.noGeo = function () {
-    $scope.$apply(function () {
-      vm.message = 'Geolocation not supported by this browser.';
-    });
-  };
+    vm.noGeo = function () {
+      $scope.$apply(function () {
+        vm.message = 'Geolocation not supported by this browser.';
+      });
+    };
 
-  geolocation.getPosition(vm.getData, vm.showError, vm.noGeo);
-}
+    geolocation.getPosition(vm.getData, vm.showError, vm.noGeo);
+  }
+})();
