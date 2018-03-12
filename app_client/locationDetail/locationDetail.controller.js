595c769d019cd90d1291f39261eaabf84ed1c12b
kwon.yuchan <kwon.yuchan@nbplus.kr>
2018-03-12 14:48:04 +0900

    add review

--- app_client/locationDetail/locationDetail.controller.js
+++ app_client/locationDetail/locationDetail.controller.js
@@ -3,19 +3,34 @@
     .module('loc8rApp')
     .controller('locationDetailCtrl', locationDetailCtrl);
   
-  locationDetailCtrl.$inject = ['$routeParams', 'loc8rData'];
-  function locationDetailCtrl($routeParams, loc8rData) {
+  locationDetailCtrl.$inject = ['$routeParams', '$uibModal', 'loc8rData'];
+  function locationDetailCtrl($routeParams, $uibModal, loc8rData) {
     var vm = this;
     vm.locationid = $routeParams.locationid;
 
     loc8rData.locationById(vm.locationid).then(function (data) {
-      console.log(data.data.name);
+      // console.log(data.data.name);
       vm.data = { location: data.data };
       vm.pageHeader = {
         title: vm.data.location.name
       };
     }).catch(function (e) {
       console.log(e);
-    })
+    });
+
+    vm.popupReviewForm = function () {
+      var uibModalInstance = $uibModal.open({
+        templateUrl: '/reviewModal/reviewModal.view.html',
+        controller: 'reviewModalCtrl as vm',
+        resolve: {
+          locationData: function () {
+            return {
+              locationid: vm.locationid,
+              locationName: vm.data.location.name
+            };
+          }
+        }
+      });
+    };
   }
 })();
---
+++ app_client/reviewModal/reviewModal.controller.js
@@ -0,0 +1,17 @@
+(function () {
+  angular
+    .module('loc8rApp')
+    .controller('reviewModalCtrl', reviewModalCtrl);
+
+  reviewModalCtrl.$inject = ['$uibModalInstance', 'locationData'];
+  function reviewModalCtrl($uibModalInstance, locationData) {
+    var vm = this;
+    vm.locationData = locationData;
+    
+    vm.modal = {
+      cancel: function () {
+        $uibModalInstance.dismiss('cancel');
+      }
+    };
+  };
+})();
