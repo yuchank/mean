ef456b64e3a3d7aed628f5e3aee002ac004bbaea
kwon.yuchan <kwon.yuchan@nbplus.kr>
2018-03-20 13:37:01 +0900

    authentication service

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ var appClientFiles = [
   'app_client/reviewModal/reviewModal.controller.js',
   'app_client/common/services/geolocation.service.js',
   'app_client/common/services/loc8rData.service.js',
+  'app_client/common/services/authentication.service.js',
   'app_client/common/filters/formatDistance.filter.js',
   'app_client/common/filters/addHtmlLineBreaks.filter.js',
   'app_client/common/filters/safeHtml.filter.js',
